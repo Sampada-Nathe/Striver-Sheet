a2a7a5d4322b30fe77400c4827e0ae3a8a4e87fe
Sampada Nathe <56882130+Sampada-Nathe@users.noreply.github.com>
2022-08-25 20:52:22 +0530

    Create pattern13.py

---
+++ pattern13.py
@@ -0,0 +1,17 @@
+rows=5
+count=0
+for i in range(rows+1):
+    for j in range(1,i+1):
+        count += 1
+        print(count,end=" ")
+    print("\r")
+
+    
+    
+"""
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15 
+"""
